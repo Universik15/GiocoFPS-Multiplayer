a8d853145c2f97fcf9c6e04c2a094008bd7bc69f
Samuel <samuel158379@hotmail.com>
2021-01-17 18:36:30 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
+    private void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+
+        RegisterPlayer();
+    }
+
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+}
